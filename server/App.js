a6846d6858c02752d7edd98423ad94975c6e1e7d
adiishappy <ad2004sahu@gmail.com>
2024-06-12 10:48:23 +0530

    added cors

--- server/App.js
+++ server/App.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+const cors = require('cors');
 const schema = require("./schema/schema");
 const mongoose = require('mongoose');
 const db = require('./db');
 const app = express();
 
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
